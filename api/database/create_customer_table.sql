bd3c37780087cf0d13677f5bc4906b2289cb4931
Gabriella Conde <conde.gabriella.r@gmail.com>
2024-03-24 21:20:20 -0400

    Refactor script

--- api/database/create_customer_table.sql
+++ api/database/create_customer_table.sql
@@ -1,8 +1,7 @@
 CREATE TABLE IF NOT EXISTS customers(
-	customer_id int UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+	customer_id int UNSIGNED ZEROFILL AUTO_INCREMENT PRIMARY KEY,
 	first_name varchar(35) NOT NULL,
 	last_name varchar(35) NOT NULL,
 	dob date NOT NULL,
-	phone_number varchar(20) NOT NULL UNIQUE,
-    PRIMARY KEY (customer_id)
+	phone_number varchar(20) NOT NULL UNIQUE
 );

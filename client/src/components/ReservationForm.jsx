a0b21ef771419de085768c9c1ae2ac9da307fbb5
zthom <zzelmanski@ufl.edu>
2024-03-26 17:21:27 -0400

    Update reservation form 3/26

--- client/src/components/ReservationForm.jsx
+++ client/src/components/ReservationForm.jsx
@@ -4,31 +4,60 @@ import './ReservationForm.css'
 function ReservationForm() {
     return (
         <form>
-            <p>Reserve a table</p>
+            <h1>Reserve a table</h1>
+            <hr></hr>
 
-            <l>Personal Information</l>
-            <label htmlFor="fname">First name:</label>
-            <input type="text" id="fname" name="fname"></input>
-            <label htmlFor="lname">Last name:</label>
-            <input type="text" id="lname" name="lname"></input>
-            <label htmlFor="dob">Date of Birth:</label>
-            <input type="date" id="dob" name="dob"></input>
+            <section>
+                <h2>Personal Information: </h2>
+            </section>
 
-            <l>Contact Information:</l>
-            <label htmlFor="phonenumber">Phone number:</label>
-            <input type="text" id="phonenumber" name="phonenumber"></input>
+            <section>
+                <label htmlFor="fname">First name: </label>
+                <input type="text" id="fname" name="fname"></input>
+                <label htmlFor="lname">Last name: </label>
+                <input type="text" id="lname" name="lname"></input>
+            </section>
 
-            <l>Reservation</l>
-            <label htmlFor="resdate">Date</label>
-            <input type="date" id="resdate" name="resdate"></input>
-            <label htmlFor="restime">Time</label>
-            <input type="time" id="restime" name="restime"></input>
-            <label htmlFor="partysize">Party Size</label>
-            <input type="number" id="partysize" name="partysize"></input>
-            <label htmlFor="table">Table</label>
-            <input type="text" id="table" name="table"></input>
+            <section>
+                
+                <label htmlFor="dob">Date of Birth: </label>
+                <input type="date" id="dob" name="dob"></input>
+            </section>
 
-            <input type="submit" value="Submit"></input>
+            <hr></hr>
+
+            <section>
+                <h2>Contact Information: </h2>
+            </section>
+
+            <section>
+                <label htmlFor="phonenumber">Phone number: </label>
+                <input type="text" id="phonenumber" name="phonenumber"></input>
+            </section>
+
+            <hr></hr>
+
+            <section>
+                <l>Reservation: </l>
+            </section>
+
+            <section>
+                <label htmlFor="resdate">Date: </label>
+                <input type="date" id="resdate" name="resdate"></input>
+                <label htmlFor="restime">Time: </label>
+                <input type="time" id="restime" name="restime"></input>
+            </section>
+
+            <section>
+                <label htmlFor="partysize">Party Size: </label>
+                <input type="number" id="partysize" name="partysize"></input>
+                <label htmlFor="table">Table: </label>
+                <input type="text" id="table" name="table"></input>
+            </section>
+
+            <hr></hr>
+
+            <input type="submit" id="Submit" name="Submit"></input>
 
         </form>
 

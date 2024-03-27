51a3e1a23b06d7164b735099fd1b3784a5405484
dsantosgonzalez <santosdiegosd@gmail.com>
2024-03-27 17:05:43 -0400

    made fields required

--- client/src/components/ReservationForm.jsx
+++ client/src/components/ReservationForm.jsx
@@ -10,11 +10,11 @@ function ReservationForm() {
             <section>
                 <h2>Personal Information</h2>
                 <label htmlFor="fname">First name:</label>
-                <input type="text" id="fname" name="fname"></input>
+                <input type="text" id="fname" name="fname" required></input>
                 <label htmlFor="lname">Last name:</label>
-                <input type="text" id="lname" name="lname"></input>
+                <input type="text" id="lname" name="lname" required></input>
                 <label htmlFor="dob">Date of Birth:</label>
-                <input type="date" id="dob" name="dob"></input>
+                <input type="date" id="dob" name="dob" required></input>
             </section>
 
             <hr></hr>
@@ -22,30 +22,29 @@ function ReservationForm() {
             <section>
                 <h2>Contact Information:</h2>
                 <label htmlFor="phonenumber">Phone number:</label>
-                <input type="text" id="phonenumber" name="phonenumber"></input>
+                <input type="text" id="phonenumber" name="phonenumber" required></input>
             </section>
 
             <hr></hr>
 
             <section>
-            <h2>Reservation</h2>
-                <label htmlFor="resdate">Date</label>
-                <input type="date" id="resdate" name="resdate"></input>
-                <label htmlFor="restime">Time</label>
-                <input type="time" id="restime" name="restime"></input>
-                <label htmlFor="partysize">Party Size</label>
-                <input type="number" id="partysize" name="partysize"></input>
-                <label htmlFor="table">Table</label>
-                <input type="time" id="table" name="table"></input>
+                <h2>Reservation</h2>
+                <section>
+                    <h2>Reservation</h2>
+                    <label htmlFor="resdate">Date</label>
+                    <input type="date" id="resdate" name="resdate" required></input>
+                    <label htmlFor="restime">Time</label>
+                    <input type="time" id="restime" name="restime" required></input>
+                    <label htmlFor="partysize">Party Size</label>
+                    <input type="number" id="partysize" name="partysize" required></input>
+                    <label htmlFor="table">Table</label>
+                    <input type="time" id="table" name="table" required></input>
+                </section>
             </section>
 
-            <hr></hr>
-
             <input type="submit" value="Submit"></input>
-
         </form>
-
-    )
+    );
 }
 
-export default ReservationForm  
+export default ReservationForm;

1c4adcf3d5ca5f267b6b41047735adb4b4ab61b8
Gabriella Conde <conde.gabriella.r@gmail.com>
2024-03-27 21:55:45 -0400

    Combine CSS

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import './HomePage.css';
+import '../App.css'
 
 const HomePage = () => {
     return (
--- client/src/components/ReservationForm.jsx
+++ client/src/components/ReservationForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './ReservationForm.css'
+import '../App.css'
 
 function ReservationForm() {
     return (

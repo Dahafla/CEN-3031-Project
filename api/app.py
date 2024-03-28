114d6ec8c07bcbb7dccf05ae703aa83a002e68e4
Gabriella Conde <conde.gabriella.r@gmail.com>
2024-03-27 21:26:32 -0400

    Connect database

--- api/app.py
+++ api/app.py
@@ -1,6 +1,32 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+import pymysql
 
 app = Flask(__name__)
+
+load_dotenv()
+db_host = os.getenv('HOST')
+db_user = os.getenv('USER')
+db_password = os.getenv('PASSWORD')
+db_name = os.getenv('DATABASE')
+
+# Connect to the database
+def connect_db():
+    connection = pymysql.connect(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        db=db_name
+    )
+    
+    cursor = connection.cursor()
+    return connection, cursor
+
+# Close connection to the database
+def close_connection(connection, cursor):
+    cursor.close()
+    connection.close()
 
 if __name__ == '__main__':
     app.run(debug=True)

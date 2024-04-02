e95db354a613ef6e6c785a0cdced5207a3e8b890
alfahad <afahad1@ufl.edu>
2024-04-02 19:32:54 -0400

    some changes to home and reservation page

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -5,10 +5,15 @@ const HomePage = () => {
     return (
         <div className="container">
             <h1 className="title">Welcome to DAGZ!</h1>
-            <p className="description">Want to make a reservation? Click the button below.</p>
-            <Link to="/reservation-form">
-                <button className="button">Go to Reservation Form</button>
-            </Link>
+            <p className="description">Want to make a reservation? Please sign in or continue as a guest.</p>
+            <div className="actions">
+                <Link to="/sign-in" className="link">
+                    <button className="button button-sign-in">Sign In</button>
+                </Link>
+                <Link to="/reservation-form" className="link">
+                    <button className="button button-guest">Continue as Guest</button>
+                </Link>
+            </div>
         </div>
     );
 };
--- client/src/components/ReservationForm.jsx
+++ client/src/components/ReservationForm.jsx
@@ -1,11 +1,55 @@
 import { useState } from 'react'
 import './ReservationForm.css'
 
+function generateTimeOptions(startHour, endHour, interval) {
+    const times = [];
+    let hour = startHour;
+    let minute = 30; // Start at 30 minutes for the 9:30 AM case
+
+    while (hour < endHour || (hour === endHour && minute === 0)) {
+        const time = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
+        // Convert 24-hour time format to 12-hour format for display
+        const time12hr = new Date(`01/01/2020 ${time}`).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true });
+        times.push({ value: time, label: time12hr });
+
+        minute += interval;
+        if (minute >= 60) {
+            minute = 0;
+            hour++;
+        }
+    }
+    return times;
+}
+
+
 function ReservationForm() {
+    const timeOptions = generateTimeOptions(9, 21, 30);
     return (
         <form>
-            <h1>Reserve a table</h1>
+            <h1>Make a reservation </h1>
             <hr></hr>
+
+            <section>
+                <h2>Reservation</h2>
+
+                <label htmlFor="resdate">Date</label>
+                <input type="date" id="resdate" name="resdate" required></input>
+
+                <label htmlFor="restime">Time</label>
+                <select id="restime" name="restime" required>
+                    {timeOptions.map(time =>
+                        <option key={time.value} value={time.value}>{time.label}</option>
+                    )}
+                </select>
+
+                <label htmlFor="partysize">Party Size</label>
+                <select id="partysize" name="partysize" required>
+                    {[...Array(20).keys()].map(num =>
+                        <option key={num + 1} value={num + 1}>{num + 1} people</option>
+                    )}
+                </select>
+            </section>
+
 
             <section>
                 <h2>Personal Information</h2>
@@ -13,8 +57,6 @@ function ReservationForm() {
                 <input type="text" id="fname" name="fname" required></input>
                 <label htmlFor="lname">Last name:</label>
                 <input type="text" id="lname" name="lname" required></input>
-                <label htmlFor="dob">Date of Birth:</label>
-                <input type="date" id="dob" name="dob" required></input>
             </section>
 
             <hr></hr>
@@ -26,19 +68,6 @@ function ReservationForm() {
             </section>
 
             <hr></hr>
-
-            <section>
-                <h2>Reservation</h2>
-
-                    <label htmlFor="resdate">Date</label>
-                    <input type="date" id="resdate" name="resdate" required></input>
-                    <label htmlFor="restime">Time</label>
-                    <input type="time" id="restime" name="restime" required></input>
-                    <label htmlFor="partysize">Party Size</label>
-                    <input type="number" id="partysize" name="partysize" required></input>
-                    <label htmlFor="table">Table</label>
-                    <input type="time" id="table" name="table" required></input>
-            </section>
 
             <input type="submit" value="Submit"></input>
         </form>
